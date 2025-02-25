580c22f4b24232967173483ac23c7175841c6a36
DavidTrota <158449092+DavidTrota@users.noreply.github.com>
2025-02-25 18:29:38 +0100

    Update ejercicios_2_6.jl
    trainingclass (terminada?)

--- ejercicios_2_6.jl
+++ ejercicios_2_6.jl
@@ -216,44 +216,82 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
     testDataset::      Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}=(Array{eltype(trainingDataset[1]),2}(undef,0,size(trainingDataset[1],2)), falses(0,size(trainingDataset[2],2))),
     transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
     maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, maxEpochsVal::Int=20)
+    
+    entradas_val, salidas_val = validationDataset
+    entradas_test, salidas_test = testDataset
+    entradas_train, salidas_train = trainingDataset
 
-    entradas, salidas = trainingDataset
-    entradas = convert(Array{Float32,2},entradas);
-    entradas_t = transpose(entradas)
-    salidas_t = transpose(salidas)
-    ann = buildClassANN(size(entradas_t)[1], topology, size(salidas_t)[2]; transferFunctions)
+    entradas_val = convert(Array{<:Float32,2}, entradas_val); 
+    entradas_test = convert(Array{<:Float32,2}, entradas_test);
+    entradas_train = convert(Array{Float32,2},entradas_train);
+    
+    entradas_val_t = transpose(entradas_val)
+    salidas_val_t = transpose(salidas_val)
+    
+    entradas_train_t = transpose(entradas_train)
+    salidas_train_t = transpose(salidas_train)
+
+    entradas_test_t = transpose(entradas_test)
+    salidas_test_t = transpose(salidas_test)
+    
+    ann = buildClassANN(size(entradas_train_t)[1], topology, size(salidas_train_t)[2]; transferFunctions)
+   
     loss(ann, x,y) = (size(y,1) == 1) ? Losses.binarycrossentropy(ann(x),y) : Losses.crossentropy(ann(x),y);
     
-    loss_total = Float32[]
-    epochs = 0
-    maxLoss = Inf
-    Val =  !(all(validationDataset -> validationDataset == 0))
+    losses_train = Float32[]
+    losses_val = Float32[];
+    losses_test = Float32[];
+
 
     optimizador = Flux.setup(Adam(learningRate), ann)
-    append!(loss_total, loss(ann,entradas_t,salidas_t))
+
+    loss_inic_train =  loss(ann,entradas_train_t,salidas_train_t)
+    push!(losses_train, loss_inic_train)
+
+    if !isempty(entradas_val)
+        loss_inic_val = loss(ann,entradas_val_t,salidas_val_t)
+        push!(losses_val, loss_inic_val)
+    end
+    
+    if !isempty(entradas_test)
+        loss_inic_test = loss(ann,entradas_test_t,salidas_test_t)
+        push!(losses_test, loss_inic_test)
+    end
+
+    best_val_loss = Inf32
+    lim_epoch_cicles = 0
+    ann_final = deepcopy(ann)
 
     for i in 1:maxEpochs
-        Flux.train!(loss, ann, [(entradas_t, salidas_t)], optimizador);
+
+        Flux.train!(loss, ann, [(entradas_train_t, salidas_train_t)], optimizador);
+        loss_train =  loss(ann,entradas_train_t,salidas_train_t)
+        push!(losses_train, loss_train)
         
-        if Val
-            loss_actual = loss(ann, inputs_val, outputs_val)
-            append!(loss_total, loss_actual)
+        if !isempty(entradas_val)
+            loss_val = loss(ann,entradas_val_t,salidas_val_t)
+            push!(losses_val, loss_val)
+            if loss_val < best_val_loss
+                best_val_loss = loss_val;
+                lim_epoch_cicles = 0;
+                ann_final = deepcopy(ann);
+            else
+                lim_epoch_cicles += 1;
 
-            epochs += 1
-            if maxLoss > loss_actual
-                epochs = 0
-                maxLoss = (loss_actual, ann)
+                if lim_epoch_cicles == 20
+                    break
+                end 
             end;
+        else
+            ann_final = ann
+        end
 
-            if loss_actual <= minLoss || epochs == maxEpochsVal 
-                break
-            end;
-        end;
-        
-    end;
-    return maxLoss
-end;
-
+        if !isempty(entradas_test)
+            push!(losses_test, loss(ann, entradas_test_t, salidas_test_t))
+        end
+    end
+    return (ann_final, losses_train, losses_val, losses_test)
+end
 function trainClassANN(topology::AbstractArray{<:Int,1},
     trainingDataset::  Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}};
     validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}=(Array{eltype(trainingDataset[1]),2}(undef,0,size(trainingDataset[1],2)), falses(0)),
